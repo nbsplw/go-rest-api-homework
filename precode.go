266b9ece154102fe96b9e1e4e24578724b758355
nbsplw <nbsplw@yandex.ru>
2024-05-04 13:13:41 +0300

    feat: homework done

--- precode.go
+++ precode.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-// Task ...
 type Task struct {
 	ID           string   `json:"id"`
 	Description  string   `json:"description"`
@@ -15,6 +16,7 @@ type Task struct {
 	Applications []string `json:"applications"`
 }
 
+// Не знаю как у вас устроены тесты, но я бы добавил мьютекс к доступу к мапе
 var tasks = map[string]Task{
 	"1": {
 		ID:          "1",
@@ -39,14 +41,85 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _, err = w.Write(data); err != nil {
+		fmt.Printf("failed to write response: %v", err)
+		return
+	}
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	var taskToAdd Task
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(r.Body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := json.Unmarshal(buf.Bytes(), &taskToAdd); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if _, ok := tasks[taskToAdd.ID]; !ok {
+		tasks[taskToAdd.ID] = taskToAdd
+	} else {
+		http.Error(w, fmt.Sprintf("task with id %s already exists", taskToAdd.ID), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTaskById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	task, exists := tasks[id]
+	if !exists {
+		http.Error(w, "task doesn't exist", http.StatusBadRequest)
+		return
+	}
+
+	data, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _, err = w.Write(data); err != nil {
+		fmt.Printf("failed to write response: %v", err)
+		return
+	}
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	_, exists := tasks[id]
+	if !exists {
+		http.Error(w, "task doesn't exist", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
-
-	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get(`/tasks`, getTasks)
+	r.Post(`/tasks`, addTask)
+	r.Get(`/tasks/{id}`, getTaskById)
+	r.Delete(`/tasks/{id}`, deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
